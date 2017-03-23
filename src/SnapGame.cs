92b9446797ad183f70353e962f3ec02b5594bc06
Madd9658 <100075105@students.swinburne.edu.my>
2017-03-23 23:51:52 +0800

    Sound Effect - Edited

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -17,40 +17,36 @@ namespace CardGames
 		/// Respond to the user input -- with requests affecting myGame
 		/// </summary>
 		/// <param name="myGame">The game object to update in response to events.</param>
-		private static void HandleUserInput(Snap myGame)
+		private static void HandleUserInput (Snap myGame)
 		{
 			//Fetch the next batch of UI interaction
-			SwinGame.ProcessEvents();
+			SwinGame.ProcessEvents ();
 
-			if (SwinGame.KeyTyped (KeyCode.vk_SPACE))
-			{
+			if (SwinGame.KeyTyped (KeyCode.vk_SPACE)) {
 				myGame.Start ();
 			}
 
-			if (myGame.IsStarted) 
+			if (myGame.IsStarted)
 			{
 				if (SwinGame.KeyTyped (KeyCode.vk_LSHIFT) &&
-<<<<<<< Updated upstream
+
 					SwinGame.KeyTyped (KeyCode.vk_RSHIFT))
-				{
-=======
-					SwinGame.KeyTyped (KeyCode.vk_RSHIFT)) {
-					SwinGame.PlaySoundEffect ("Slap");
->>>>>>> Stashed changes
+				 {
+						SwinGame.PlaySoundEffect ("Slap");
 					//TODO: add sound effects 
 
 				} 
 				else if (SwinGame.KeyTyped (KeyCode.vk_LSHIFT)) 
-				{
-					myGame.PlayerHit (0);
-				}
-
-				else if (SwinGame.KeyTyped (KeyCode.vk_RSHIFT))
-				{
-					myGame.PlayerHit (1);
+					{
+						myGame.PlayerHit (0);
+					} 
+					else if (SwinGame.KeyTyped (KeyCode.vk_RSHIFT))
+					{
+						myGame.PlayerHit (1);
+					}
 				}
 			}
-		}
+
 
 		/// <summary>
 		/// Draws the game to the Window.
