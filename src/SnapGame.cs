0871dc6cb2f1d11c7078d8b31a97789d2a56f081
khaled2095 <100082095@students.swinburne.edu.my>
2017-03-24 14:44:05 +0800

    cards are over the background fixed

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -65,7 +65,7 @@ namespace CardGames
 				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
 				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
-				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 350, 50);
+				//SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 350, 50);
 			}
 			else
 			{
